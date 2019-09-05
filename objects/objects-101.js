93a55b7384d9fc6b096118a67c1f0a6c0510d4f7
Filbert Mendoza <fmendoza@malwarebytes.com>
2019-09-04 17:40:58 -0700

    Started objects section

---
+++ objects/objects-101.js
@@ -0,0 +1,30 @@
+let myBook = {
+  title: '1984',
+  author: 'George Orwell',
+  pageCount: 326
+}
+
+console.log(`${myBook.title} by ${myBook.author}`)
+
+myBook.title = 'Animal Farm'
+
+console.log(`${myBook.title} by ${myBook.author}`)
+
+
+// Challenge area
+
+// name, age, location
+
+// Andrew is 27 and lives in Philadelphia.
+// Increase age by 1 and print message again.
+
+let person = {
+  name: 'Andrew',
+  age: 27,
+  location: 'Philadelphia'
+}
+
+console.log(`${person.name} is ${person.age} and lives in ${person.location}`)
+
+person.age += 1
+console.log(`${person.name} is ${person.age} and lives in ${person.location}`)
---
+++ objects/objects-functions.js
@@ -0,0 +1,39 @@
+let myBook = {
+  title: '1984',
+  author: 'George Orwell',
+  pageCount: 326
+}
+
+let otherBook = {
+  title: 'A Peoples History',
+  author: 'Howard Zin',
+  pageCount: 723
+}
+
+function getSummary(book) {
+  return {
+    summary: `${book.title} by ${book.author}`,
+    pageCountSummary: `${book.title} is ${book.pageCount} pages long`
+  }
+}
+
+let bookSummary = getSummary(myBook)
+let otherBookSummary = getSummary(otherBook)
+
+console.log(bookSummary.pageCountSummary)
+
+// Challenge area
+// Create function - take fahrenheit in - return object with all three
+
+function convertFahrenheit(fahrenheit) {
+  let tempC = (fahrenheit - 32) * (5 / 9)
+  let tempK = (fahrenheit + 459.67) * (5 / 9)
+
+  return {
+    fahrenheit: fahrenheit,
+    celsius: tempC,
+    kelvin: tempK
+  }
+}
+
+console.log(convertFahrenheit(74))
